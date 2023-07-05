ed9274b9925d3b8d4ad10cbbf1fdef9aeb29655c
Robin Dickson <dic09005@byui.edu>
2023-07-05 16:44:19 -0600

    commit

--- __tests__/admin.test.js
+++ __tests__/admin.test.js
@@ -8,12 +8,32 @@ describe('Test Handlers', () => {
     test('responds to /', async () => {
         const res = await request.get('/');
         expect(res.header['content-type']).toBe('text/html; charset=utf-8');
-        expect(res.statusCode).toBe(404)
+        expect(parseInt(res.header['content-length'])).toBeLessThan(1000);
+
     })
 
     test('responds to /', async () => {
-        const res = await request.get('/');
-        expect(res.header['content-type']).toBe('text/html; charset=utf-8');
-        expect(res.statusCode).toBe(404)
+        const res = await request.get('/admin/61a921f6028954d4f0319e6d');
+        expect(res.header['content-type']).toBe('application/json; charset=utf-8');
+        expect(parseInt(res.header['content-length'])).toBeLessThan(1000);
+
+    })
+
+    test('responds to /', async () => {
+        const res = await request.get('/employee');
+        expect(res.header['content-type']).toBe('application/json; charset=utf-8');
+        expect(parseInt(res.header['content-length'])).toBeLessThan(1000);
+    })
+
+    test('responds to /', async () => {
+        const res = await request.get('/patient');
+        expect(res.header['content-type']).toBe('application/json; charset=utf-8');
+        expect(parseInt(res.header['content-length'])).toBeLessThan(1000);
+    })
+
+    test('responds to /', async () => {
+        const res = await request.get('/schedule');
+        expect(res.header['content-type']).toBe('application/json; charset=utf-8');
+        expect(parseInt(res.header['content-length'])).toBeLessThan(1000);
     })
 })
