3476b884a020130ba7e227486367294d37d5a292
Robin Dickson <dic09005@byui.edu>
2023-07-05 17:00:35 -0600

    commit

--- __tests__/admin.test.js
+++ __tests__/admin.test.js
@@ -1,39 +1,36 @@
-const app = require('../app')
-const supertest = require('supertest');
-const { expect } = require('@jest/globals');
-const request = supertest(app)
+const app = require("../app");
+const supertest = require("supertest");
+const { expect } = require("@jest/globals");
+const request = supertest(app);
 
+describe("Test Handlers", () => {
+  test("responds to /", async () => {
+    const res = await request.get("/");
+    expect(res.header["content-type"]).toBe("text/html; charset=utf-8");
+    expect(parseInt(res.header["content-length"])).toBeLessThan(1000);
+  });
 
-describe('Test Handlers', () => {
-    test('responds to /', async () => {
-        const res = await request.get('/');
-        expect(res.header['content-type']).toBe('text/html; charset=utf-8');
-        expect(parseInt(res.header['content-length'])).toBeLessThan(1000);
+  test("responds to /", async () => {
+    const res = await request.get("/admin/61a921f6028954d4f0319e6d");
+    expect(res.header["content-type"]).toBe("application/json; charset=utf-8");
+    expect(parseInt(res.header["content-length"])).toBeLessThan(1000);
+  });
 
-    })
+  test("responds to /", async () => {
+    const res = await request.get("/employee");
+    expect(res.header["content-type"]).toBe("application/json; charset=utf-8");
+    expect(parseInt(res.header["content-length"])).toBeLessThan(1000);
+  });
 
-    test('responds to /', async () => {
-        const res = await request.get('/admin/61a921f6028954d4f0319e6d');
-        expect(res.header['content-type']).toBe('application/json; charset=utf-8');
-        expect(parseInt(res.header['content-length'])).toBeLessThan(1000);
+  test("responds to /", async () => {
+    const res = await request.get("/patient");
+    expect(res.header["content-type"]).toBe("application/json; charset=utf-8");
+    expect(parseInt(res.header["content-length"])).toBeLessThan(1000);
+  });
 
-    })
-
-    test('responds to /', async () => {
-        const res = await request.get('/employee');
-        expect(res.header['content-type']).toBe('application/json; charset=utf-8');
-        expect(parseInt(res.header['content-length'])).toBeLessThan(1000);
-    })
-
-    test('responds to /', async () => {
-        const res = await request.get('/patient');
-        expect(res.header['content-type']).toBe('application/json; charset=utf-8');
-        expect(parseInt(res.header['content-length'])).toBeLessThan(1000);
-    })
-
-    test('responds to /', async () => {
-        const res = await request.get('/schedule');
-        expect(res.header['content-type']).toBe('application/json; charset=utf-8');
-        expect(parseInt(res.header['content-length'])).toBeLessThan(1000);
-    })
-})
+  test("responds to /", async () => {
+    const res = await request.get("/schedule");
+    expect(res.header["content-type"]).toBe("application/json; charset=utf-8");
+    expect(parseInt(res.header["content-length"])).toBeLessThan(1000);
+  });
+});
